059ba0773385403f68f7adba4746a1cb2b06a207
Anton Voskanyan <antonvoskanan@macserver.local>
2022-02-22 19:13:35 +0300

    fix imports

---
+++ src/components/index.js
@@ -0,0 +1,6 @@
+import { SearchInput } from './SearchInput';
+import { Table } from './Table';
+import { NavigationBar } from './NavigationBar';
+import { AddButton } from './AddButton';
+
+export { SearchInput, Table, NavigationBar, AddButton };
