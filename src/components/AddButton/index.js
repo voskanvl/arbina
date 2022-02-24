00abce5427b1ee764a3e12768341ad77c75f3d57
Anton Voskanyan <antonvoskanan@macserver.local>
2022-02-24 22:47:44 +0300

    fix name in button

--- src/components/AddButton/index.js
+++ src/components/AddButton/index.js
@@ -5,10 +5,10 @@ import {useDispatch, useSelector} from "react-redux";
 export const AddButton = memo((...props) => {
     const value = useSelector(state => state.currentValue);
     const dispatch = useDispatch();
-    const habdlerAdd = useCallback(() => {
+    const handlerAdd = useCallback(() => {
         dispatch({type: 'ADD', payload: value});
         dispatch({type: 'CURRENT_VALUE', payload: ""});
     }, [dispatch, value]);
-    return <Button className="bp3-minimal" icon="plus" onClick={habdlerAdd} {...props} />
+    return <Button className="bp3-minimal" icon="plus" onClick={handlerAdd} {...props} />
 })
 
